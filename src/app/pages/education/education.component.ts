2d6376da27f0dbbd79d28ebdaea7fe467d846627
StefanoStone <stefanocamp187@gmail.com>
2023-07-23 18:33:43 +0200

    update education

--- src/app/pages/education/education.component.ts
+++ src/app/pages/education/education.component.ts
@@ -11,13 +11,13 @@ export class EducationComponent {
   degrees: ListItem[] = [
     {
       title: 'Master in Software and Data Engineering',
-      subtitle: 'Università della Svizzera Italiana | 2022 - ongoing',
+      subtitle: 'Università della Svizzera italiana | 2022 - 2023',
       description: 'Joint degree with University of Molise'
     },
     {
       title: 'Master in Software Systems Security',
-      subtitle: 'University of Molise | 2021 - ongoing',
-      description: 'Joint degree with Università della Svizzera Italiana'
+      subtitle: 'University of Molise | 2021 - 2023',
+      description: 'Joint degree with Università della Svizzera italiana'
     },
     {
       title: 'Bachelor in Computer Science',
@@ -32,6 +32,12 @@ export class EducationComponent {
       subtitle: 'University of Molise | 2021',
       description: 'Thesis in the course of Hands on Machine Learning for the prediction of blood pressure values with ' +
         'the use of a mobile application and the support of a smartwatch with a PPG sensor built-in'
+    },
+    {
+      title: 'RePlay: Replicating Gameplay through Video Analysis and On-Screen Controls',
+      subtitle: 'University of Molise & Università della Svizzera italiana | 2023',
+      description: 'Thesis in the course of Software Analytics for the replication of gameplay through the analysis of ' +
+        'video recordings by exploiting on-screen controls in the gameplay; Proof of Concept with the game Trackmania.'
     }
   ]
 
