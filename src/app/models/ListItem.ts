84213c9f950cc8d2be6aba4244c4b81617f1e7d3
StefanoStone <stefanocamp187@gmail.com>
2023-10-07 10:29:22 +0200

    Auto-generated commit [ci skip]

--- src/app/models/ListItem.ts
+++
@@ -1,5 +0,0 @@
-export interface ListItem {
-  title: string;
-  subtitle: string;
-  description: string;
-}
--- src/app/models/Tabs.ts
+++
@@ -1,6 +0,0 @@
-export enum Tabs {
-  about = 0,
-  resume = 1,
-  education = 2,
-  contacts = 3
-}
