30a75658fc480df8ed6cce2314770105726d2b5e
Stefano Campanella <stefano.campanella@usi.ch>
2024-08-29 17:13:27 +0200

    move data to json files

--- src/app/pages/resume/resume.component.ts
+++ src/app/pages/resume/resume.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import {ListItem} from "../../models/ListItem";
+import {DataService} from "../../services/data.service";
+import {DataPaths} from "../../models/dataPaths";
 
 @Component({
   selector: 'app-resume',
@@ -8,117 +10,32 @@ import {ListItem} from "../../models/ListItem";
 })
 export class ResumeComponent {
 
-  projects: ListItem[] = [
-    {
-      title: 'Front-End Developer',
-      subtitle: 'Università della Svizzera italiana | October 2022 - January 2023',
-      description: 'Development of a web portal to track travels of USI employees'
-    },
-    {
-      title: 'Machine Learning Engineer',
-      subtitle: 'Università della Svizzera italiana | October 2022 - January 2023',
-      description: 'Development of a machine learning model for the prediction of issue assignee in github and ' +
-        'prediction of method name in Java'
-    },
-    {
-      title: 'Front-End Project Manager',
-      subtitle: 'University of Molise | March 2022 - July 2022',
-      description: 'Team manager for the development of "App Unimol”, an application used by university students'
-    },
-    {
-      title: 'Android app design and development',
-      subtitle: 'University of Molise | October 2020 – January 2021',
-      description: 'Development and design of a prototype in Android of an app in the area of video content ' +
-        'streaming'
-    },
-    {
-      title: 'Hybrid architecture app design and development',
-      subtitle: 'University of Molise | March 2020 – July 2020',
-      description: 'Front-end team member for development of UniMove, hybrid app based on Ionic-Angular framework'
-    },
-    {
-      title: 'Java videogame development',
-      subtitle: 'University of Molise | March 2019 - July 2019',
-      description: 'Development of a videogame in Java for learning purposes'
-    },
-    {
-      title: 'Management program in C language',
-      subtitle: 'University of Molise | October 2018 – January 2019',
-      description: 'Development of a program in C language for the management of a Apple device repair company'
-    }
-  ];
+  projects: ListItem[] = [];
+  degrees: ListItem[] = [];
+  theses: ListItem[] = [];
+  workingExperience: ListItem[] = [];
+  teachingExperience: ListItem[] = [];
+  dataPaths = DataPaths;
 
-  degrees: ListItem[] = [
-    {
-      title: 'PhD in Computer Science/Software Engineering',
-      subtitle: 'Università della Svizzera italiana | 2023 - ongoing',
-      description: 'REVEAL (Reverse Engineering, Visualization, Evolution Analysis Lab) group'
-    },
-    {
-      title: 'Master in Software and Data Engineering',
-      subtitle: 'Università della Svizzera italiana | 2022 - 2023',
-      description: 'Joint degree with University of Molise'
-    },
-    {
-      title: 'Master in Software Systems Security',
-      subtitle: 'University of Molise | 2021 - 2023',
-      description: 'Joint degree with Università della Svizzera italiana'
-    },
-    {
-      title: 'Bachelor in Computer Science',
-      subtitle: 'University of Molise | 2018 - 2021',
-      description: 'Graduated defending a thesis on Machine Learning'
-    }
-  ];
+  constructor(private dataService: DataService) {
+    this.dataService.loadListItems(this.dataPaths.projects, (data: ListItem[]) => {
+      this.projects = data;
+    });
 
-  theses: ListItem[] = [
-    {
-      title: 'A methodology and an App for measuring blood pressure',
-      subtitle: 'University of Molise | 2021',
-      description: 'Thesis in the course of Hands on Machine Learning for the prediction of blood pressure values with ' +
-        'the use of a mobile application and the support of a smartwatch with a PPG sensor built-in'
-    },
-    {
-      title: 'RePlay: Replicating Gameplay through Video Analysis and On-Screen Controls',
-      subtitle: 'University of Molise & Università della Svizzera italiana | 2023',
-      description: 'Thesis in the course of Software Analytics for the replication of gameplay through the analysis of ' +
-        'video recordings by exploiting on-screen controls in the gameplay; Proof of Concept with the game Trackmania.'
-    }
-  ]
+    this.dataService.loadListItems(this.dataPaths.degrees, (data: ListItem[]) => {
+      this.degrees = data;
+    });
 
-  workingExperience: ListItem[] = [
-    {
-      title: 'Front-End Developer',
-      subtitle: 'Datasound SRL | March 2022 - January 2023',
-      description: 'Maintenance and development of a web portal and mobile application for document sharing'
-    },
-    {
-      title: 'Research Fellowship',
-      subtitle: 'University of Molise | March 2022 - August 2022',
-      description: 'Design and development of coordination tools aimed at implementing conservation objectives and ' +
-        'measures in Natura 2000 sites included in Reserves and other state-owned areas managed by the Carabinieri'
-    },
-    {
-      title: 'Front-End Project Manager',
-      subtitle: 'Associazione Perfetta Letizia | October 2021 - December 2021',
-      description: 'Team manager for the development of "Care for You”, an application used by ' +
-        'Associazione Perfetta Letizia to support seniors in the community'
-    }
-  ];
+    this.dataService.loadListItems(this.dataPaths.theses, (data: ListItem[]) => {
+      this.theses = data;
+    });
 
-  teachingExperience: ListItem[] = [
-    {
-      title: 'Teaching Assistant',
-      subtitle: 'USI | September 2023 - January 2024',
-      description: 'Course: Engineering of Domain Specific Languages'
-    },
-    {
-      title: 'Teaching Assistant',
-      subtitle: 'USI | March 2024 - May 2024',
-      description: 'Course: Software Atelier 4'
-    }
-  ];
+    this.dataService.loadListItems(this.dataPaths.workingExperience, (data: ListItem[]) => {
+      this.workingExperience = data;
+    });
 
-  constructor() {
+    this.dataService.loadListItems(this.dataPaths.teachingExperience, (data: ListItem[]) => {
+      this.teachingExperience = data;
+    });
   }
 }
